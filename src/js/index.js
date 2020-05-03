7112e328804f5eda7b9b875c27fbcf87ecc2e45e
aakashsr <aakashsri.dev@gmail.com>
2020-05-03 05:58:32 +0530

    added state and a event handler function that will do the required task as soon as submit btn is clicked

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,28 @@ import Search from "./models/Search";
 const search = new Search("pizza");
 console.log(search);
 
-search.getResults();
-// console.log(recipeResult);
+const state = {};
+
+const controlSearch = async () => {
+  // 1. get the query from the view
+  const query = "pizza";
+
+  //  if query exist
+  if (query) {
+    // 2. create a new search object with help of this query and save in state
+    state.search = new Search(query);
+
+    // 3. Prepared UI from results
+
+    // 4. Make the search
+    await state.search.getResults();
+
+    // 5. console the results for now
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
