87e65115aa02b3aab2a00297a033196e3e0fb77e
aakashsr <aakashsri.dev@gmail.com>
2020-05-05 08:10:36 +0530

    created a function to limit the recipe title upto 17 char

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -10,15 +10,31 @@ export const clearResults = () => {
   elements.searchRecipeList.innerHTML = "";
 };
 
+const limitRecipeTitle = function (title, limit = 17) {
+  const newTitle = [];
+  if (title.length > 17) {
+    title.split(" ").reduce((acc, cur) => {
+      if (acc + cur.length < limit) {
+        newTitle.push(cur);
+      }
+      return acc + cur.length;
+    }, 0);
+    return `${newTitle.join(" ")}...`;
+  }
+  return title;
+};
+
 const renderRecipe = (recipe) => {
   var markup = `
     <li>
-      <a class="results__link results__link--active" href="#${recipe.recipe_id}">
+      <a class="results__link results__link--active" href="#${
+        recipe.recipe_id
+      }">
         <figure class="results__fig">
             <img src="${recipe.image_url}" alt= ${recipe.title}>
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
       </a>
